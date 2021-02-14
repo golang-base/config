cd0f4ac2939f86a6fd02a18c0e253fcf3753786a
小二狼 <appx@outlook.com>
2021-02-14 22:15:32 +0800

    up

--- config.go
+++ config.go
@@ -1,12 +1,45 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+)
 
+var Config _Config
 
-func TestConfig(){
-	fmt.Println("i am config test")
+func InitConfig() {
+	viper.SetConfigName("dev")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("./conf/")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("read config failed: %v", err)
+	}
+
+	viper.Unmarshal(&Config)
+
+	fmt.Println(Config.MySQL)
 }
 
-func main() {
-	fmt.Printf("12322222222")
+type _Config struct {
+	AppName  string `mapstructure:"app_name"`
+	LogLevel string `mapstructure:"log_Level"`
+
+	MySQL _MySQLConfig
+	Redis _RedisConfig
 }
+
+type _MySQLConfig struct {
+	IP       string
+	Port     int
+	User     string
+	Password string
+	Database string
+}
+
+type _RedisConfig struct {
+	IP   string
+	Port int
+}
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module "github.com/golang-base/config"
+module github.com/golang-base/config
 
 go 1.15
+
+require github.com/spf13/viper v1.7.1
